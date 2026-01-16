cbc639d31b53ea5c09957534c44b13c99117369c
akshaymoliya <akshaymoliya1234@gmail.com>
2026-01-16 16:15:39 +0530

    feat: add multiple favicon sizes and an Apple Touch icon with corresponding Nuxt configuration updates.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,9 +27,12 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon-512x512.png' },
       ],
     },
   },
